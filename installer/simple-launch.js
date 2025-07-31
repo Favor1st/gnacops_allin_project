a9167014a7a9e87050ebfc47e96b3f1dcd9814cc
Favor1st <favorlemuelgrigsby@gmail.com>
2025-07-31 23:06:57 +0100

    Fix ES module syntax in simple launcher

--- installer/simple-launch.js
+++ installer/simple-launch.js
@@ -1,8 +1,12 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
-const { promisify } = require('util');
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const execAsync = promisify(exec);
 
